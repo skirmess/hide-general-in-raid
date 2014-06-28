5561cb7dbaa4e8232f3d09eb23d4e8e162017d30
Sven Kirmess <sven.kirmess@kzone.ch>
2014-06-28 14:08:34 +0000

    v7
    git-svn-id: https://hide-general-in-raid.googlecode.com/svn/trunk/tags/HideGeneralInRaid-7@20 230dd51a-4dcf-d936-6ad4-e94608466a8b

---
+++ HideGeneralInRaid/HideGeneralInRaid.lua
@@ -0,0 +1,45 @@
+
+-- Copyright (c) 2010-2014, Sven Kirmess
+
+local Version = 7
+local Loaded = false
+
+local function log(msg)
+
+	if ( msg == nil ) then
+		return
+	end
+
+	DEFAULT_CHAT_FRAME:AddMessage("Hide General In Raid: "..msg)
+end
+
+local function HideOrShowGeneral()
+
+	local inInstance, instanceType = IsInInstance()
+	if ( ( inInstance ) and ( instanceType == "raid" ) ) then
+		-- log(string.format("%s channel hidden", GENERAL))
+		ChatFrame_RemoveChannel(DEFAULT_CHAT_FRAME, GENERAL)
+	else
+		-- log(string.format("%s channel shown", GENERAL))
+		ChatFrame_AddChannel(DEFAULT_CHAT_FRAME, GENERAL)
+	end
+end
+
+local function EventHandler(self, event, ...)
+
+	if ( event == "PLAYER_ENTERING_WORLD" ) then
+
+		if ( not Loaded ) then
+			log(string.format("Version %i loaded.", Version))
+			Loaded = true
+		end
+
+		-- log("PLAYER_ENTERING_WORLD")
+		HideOrShowGeneral()
+	end
+end
+
+-- main
+local frame = CreateFrame("Frame")
+frame:SetScript("OnEvent", EventHandler)
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
---
+++ HideGeneralInRaid/readme.txt
@@ -0,0 +1,25 @@
+
+Hide General In Raid
+
+Hides the general chat channel when you're inside a raid dungeon
+
+*** Changelog
+
+Version 6
+ * Updated TOC for WoW 5.4.0
+ * CHAT_MSG_CHANNEL_NOTICE is no longer fired if you enter or leave an instance eventhought the general channel changes to the instance channel. We will now change the channel on PLAYER_ENTERING_WORLD, which is fired for every loading screen. The addon will no longer update the visibility of the General channel if you manually leave or join the channel until the next loading screen.
+
+Version 6
+ * Updated TOC for WoW 5.2.0
+
+Version 5
+ * Updated for MoP.
+
+Version 4
+ * Updated TOC for WoW 4.3.0
+
+Version 3
+ * Updated TOC for WoW 4.0.1
+
+Version 2
+ * Removed debug output
