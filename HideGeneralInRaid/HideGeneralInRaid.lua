e477cc7cbc28e156a428d02a773686700f076672
Sven Kirmess <sven.kirmess@kzone.ch>
2014-06-28 14:08:01 +0000

    v7 CHAT_MSG_CHANNEL_NOTICE is no longer fired if you enter or leave an instance eventhought the general channel changes to the instance channel. We will now change the channel on PLAYER_ENTERING_WORLD, which is fired for every loading screen. The addon will no longer update the visibility of the General channel if you manually leave or join the channel until the next loading screen.
    git-svn-id: https://hide-general-in-raid.googlecode.com/svn/trunk/trunk@19 230dd51a-4dcf-d936-6ad4-e94608466a8b

--- HideGeneralInRaid/HideGeneralInRaid.lua
+++ HideGeneralInRaid/HideGeneralInRaid.lua
@@ -1,7 +1,7 @@
 
--- Copyright (c) 2010-2013, Sven Kirmess
+-- Copyright (c) 2010-2014, Sven Kirmess
 
-local Version = 6
+local Version = 7
 local Loaded = false
 
 local function log(msg)
@@ -13,9 +13,19 @@ local function log(msg)
 	DEFAULT_CHAT_FRAME:AddMessage("Hide General In Raid: "..msg)
 end
 
-local function EventHandler(self, event, ...)
+local function HideOrShowGeneral()
 
-	local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
+	local inInstance, instanceType = IsInInstance()
+	if ( ( inInstance ) and ( instanceType == "raid" ) ) then
+		-- log(string.format("%s channel hidden", GENERAL))
+		ChatFrame_RemoveChannel(DEFAULT_CHAT_FRAME, GENERAL)
+	else
+		-- log(string.format("%s channel shown", GENERAL))
+		ChatFrame_AddChannel(DEFAULT_CHAT_FRAME, GENERAL)
+	end
+end
+
+local function EventHandler(self, event, ...)
 
 	if ( event == "PLAYER_ENTERING_WORLD" ) then
 
@@ -24,48 +34,8 @@ local function EventHandler(self, event, ...)
 			Loaded = true
 		end
 
-		self:RegisterEvent("CHAT_MSG_CHANNEL_NOTICE")
-
-	elseif ( event == "PLAYER_LEAVING_WORLD" ) then
-
-		self:UnregisterEvent("CHAT_MSG_CHANNEL_NOTICE")
-
-	elseif ( event == "CHAT_MSG_CHANNEL_NOTICE" ) then
-
-		-- Fired when you enter or leave a chat channel (or a channel was
-		-- recently throttled)
-		--
-		-- arg1		type
-		--		"YOU_JOINED" if you joined a channel
-		--		"YOU_LEFT" if you left
-		--		"THROTTLED" if channel was throttled
-		--
-		-- arg4		Channel name with number (e.g. "6. TestChannel")
-		--
-		-- arg7		Channel Type (e.g. 0 for any user channel,
-		--		1 for system-channel "General", 2 for "Trade")
-		--
-		-- arg8		Channel Number
-		--
-		-- arg9		Channel name without number
-
-		-- CHAT_MSG_CHANNEL_NOTICE(arg1, arg7, arg8, arg9)
-		if ( not arg1 or not arg7 ) then
-			log("Ignoring malformed CHAT_MSG_CHANNEL_NOTICE event.")
-			return
-		end
-
-		if ( arg7 == 1 ) then
-			-- General
-			if (( arg1 == "YOU_JOINED" ) or ( arg1 == "YOU_CHANGED" ) ) then
-				local inInstance, instanceType = IsInInstance()
-				if ( ( inInstance ) and ( instanceType == "raid" ) ) then
-					ChatFrame_RemoveChannel(DEFAULT_CHAT_FRAME, "General")
-				else
-					ChatFrame_AddChannel(DEFAULT_CHAT_FRAME, "General")
-				end
-			end
-		end
+		-- log("PLAYER_ENTERING_WORLD")
+		HideOrShowGeneral()
 	end
 end
 
@@ -73,5 +43,3 @@ end
 local frame = CreateFrame("Frame")
 frame:SetScript("OnEvent", EventHandler)
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame:RegisterEvent("PLAYER_LEAVING_WORLD")
-
